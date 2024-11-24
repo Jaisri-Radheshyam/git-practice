eed570919df096bb0422813a24233ee42b7ea79f
jayasris <saluva.jayasri@gmail.com>
2024-11-24 12:21:22 +0530

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,11 @@
 
 echo "Please enter your user name::"
 
-read USERNAME
+read -s USERNAME #takes input into USERNAME variable
 
 echo "Username entered is: $USERNAME"
+
+echo "Please enter your password::"
+
+read -s PASSWORD
+echo "password entered is: $password"
