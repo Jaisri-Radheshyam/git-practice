2e27c93f47f2f098b50b3fba219e44f3b9d7208b
jayasris <saluva.jayasri@gmail.com>
2024-11-26 08:40:03 +0530

    funactions

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
 USERID=$(id -u)
-echo "User ID is: $USERID"
+#echo "User ID is: $USERID"
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root priveleges"
+    exit 1
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y
+else
+    echo "Git is already installed, nothing to do.."
+fi
+
