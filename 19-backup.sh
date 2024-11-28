32889eb52fdc017b4b669a5dd448ba920c70af38
jayasris <saluva.jayasri@gmail.com>
2024-11-28 09:38:51 +0530

    functions

--- 19-backup.sh
+++ 19-backup.sh
@@ -17,4 +17,23 @@ USAGE(){
 if [ $# -lt 2 ]
 then
     USAGE
+fi
+
+if  [ ! -d $SOURCE_DIR ]
+then
+    echo "$SOURCE_DIR doest not exist...Please check"
+fi
+
+if  [ ! -d $DEST_DIR ]
+then
+    echo "$SOURCE_DIR doest not exist...Please check"
+fi
+
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
+
+if [ -n $FILES ]
+then
+    echo "Files are found"
+else
+    echo "No files older than $DAYS"
 fi    
